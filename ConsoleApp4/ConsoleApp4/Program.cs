5956c22b1118353522fcece0de963569a2948d70
Olivia Croteau (RIT Student) <111761829+oliviacroteau667@users.noreply.github.com>
2024-09-24 09:07:34 -0400

    Initial commit

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int myFavNum = 3;
+            int bestNum = 5;
+
+            int sum = myFavNum + bestNum;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
